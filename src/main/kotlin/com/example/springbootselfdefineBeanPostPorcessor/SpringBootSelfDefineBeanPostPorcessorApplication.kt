110c74a11c47ccc4a7ba64d262a9ab7d017f5136
Cnfn <cnfnss@gmail.com>
2019-06-16 22:11:12 +0800

    init

---
+++ src/main/kotlin/com/example/springbootselfdefineBeanPostPorcessor/SpringBootSelfDefineBeanPostPorcessorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootselfdefineBeanPostPorcessor
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootSelfDefineBeanPostPorcessorApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(SpringBootSelfDefineBeanPostPorcessorApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/example/springbootselfdefineBeanPostPorcessor/SpringBootSelfDefineBeanPostPorcessorApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.springbootselfdefineBeanPostPorcessor
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootSelfDefineBeanPostPorcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
